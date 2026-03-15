44951f41be804ca3258669a70c3a850b0309d4da
Mushfiq <mushfiq2808@gmail.com>
2026-03-15 22:06:34 +0600

    fix(main): update CORS and runtime version

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ app = FastAPI(
 # CORS — allow the Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://ielts-practice-hub-nine.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
